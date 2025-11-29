3d0d72a63fcc00788712c4dad322de37ddfae44a
山月秋分 <aideaaa@outlook.com>
2025-11-29 17:17:03 +0800

    路由改名

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -4,12 +4,12 @@ Routes Package - API 路由模块
 所有 Flask Blueprint 路由都在这里注册
 """
 
-from .bridge import bridge_bp
-from .style import style_bp
-from .structure import structure_bp
-from .production import production_bp
-from .outline import outline_bp
-from .agent_usage import agent_usage_bp
+from .routes_bridge import bridge_bp
+from .routes_style import style_bp
+from .routes_structure import structure_bp
+from .routes_production import production_bp
+from .routes_outline import outline_bp
+from .routes_agent_usage import agent_usage_bp
 
 __all__ = [
     'bridge_bp',
