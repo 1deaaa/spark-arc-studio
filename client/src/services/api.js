38550441add9220210622ebeffb2e732f06a21e4
山月秋分 <aideaaa@outlook.com>
2026-01-03 04:10:47 +0800

    HTTP受着呗

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -10,7 +10,6 @@ export * from './storyService';
 export * from './aiService';
 export * from './chatService';
 export * from './adminService';
-export * from './cryptoService';
 
 // 特殊导出（如果某些老代码显式使用了 default）
 import { fetchWithAuth } from './apiClient';
